d3cf5a4f006efad39e69212f5361b6a09e053321
jonathan <bayart.jonathan@laposte.net>
2023-02-19 15:38:51 +0100

    useState sur input search

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Card from './components/Card';
 const App = () => {
 
   const [mealsData, setMealsData] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
 
   useEffect(() => {
-    axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=tomato')
+    axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + inputSearch)
     .then((res) => setMealsData(res.data.meals));
-  })
+  }, [inputSearch]);
 
   return (
     <div className='app-container'>
       <h1>React cooking App</h1>
-      <input type="text" placeholder="Taper le nom d'un aliment(en anglais)"></input>
+      <input type="text" placeholder="Taper le nom d'un aliment(en anglais)"
+      onChange={(e) => setInputSearch(e.target.value)}
+    />
 
       <div className='meals-container'>
         {mealsData.map((meal) => 
