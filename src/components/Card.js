eabe39443fbedcc9b5de7b4fbdccb18de30b3993
jonathan <bayart.jonathan@laposte.net>
2023-02-19 15:16:46 +0100

    ajout style

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,6 +6,8 @@ const Card = ({ meal }) => {
             <h2>{meal.strMeal}</h2>
             <p>Origin : {meal.strArea}</p>
             <img src={meal.strMealThumb} alt={meal.strMeal}/>
+            <p>{meal.strInstructions}</p>
+            <a href={meal.strYoutube}> Voir la recette</a>
         </div>
     );
 };
